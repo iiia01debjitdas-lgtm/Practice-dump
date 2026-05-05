c6c0885bc8a2c6938a330112380a9ef892aedb3a
Debjit <iiia01debjitdas@gmail.com>
2026-05-05 11:10:13 +0530

    Learning Data types

---
+++ Data-types/script.js
@@ -0,0 +1,15 @@
+const name = "Debjit Das";
+const greet = "Helloo, "
+const greetings = `${greet}${name}, Remember that "All Road Leads to Rome"`;
+alert(greetings)
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = alphabet.length;
+alert(length);
+let letter = alphabet.at(-1);
+alert(letter);
+let part = alphabet.slice(0,6);
+alert(part);    
+let lower = alphabet.toLowerCase();
+alert(lower);
+let nan = name.replace("Debjit", "Trisha");
+alert(nan);
